5bf09d2902a159cab2749d5e1d1c9f3181e6149d
SiteshPrasad Mishra <spmishra@amazon.com>
2024-04-04 19:14:10 -0400

    fixed typo for models

--- models/order.js
+++ models/order.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const orderSchema = new Schema({
-    orderId : {type:Number},
-    orderStatus: {type:String, default:"Created"},
+    OrderId : {type:Number},
+    OrderStatus: {type:String, default:"Created"},
     CustomerDetails: {
         ID: {type:Number},
         Email: {type: String},
